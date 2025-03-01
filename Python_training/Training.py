8dd1d57175147864d82605bb65235aa46b51acd0
Jon <181170986+jongreg288@users.noreply.github.com>
2025-03-01 11:45:22 -0600

    NaN

--- Python_training/Training.py
+++ Python_training/Training.py
@@ -2,7 +2,6 @@ import math
 import datetime
 import random
 
-'''
 print("Hello, World!")
 
 x = 5
@@ -60,17 +59,17 @@ print("The numbers generated are " + str(list_44)) # prints the numbers generate
 print("The number of times each number is generated is as follows:") # prints the frequency of each number
 for i in set(list_44):
     print(str({i: list_44.count(i)}))
-'''
+
 # clear() - removes all elements from the list
-#del - removes the item at the specified index
-#del list_name - deletes the list
-#pop() - removes the last item from the list
-#pop(index) - removes the item at the specified index
-#remove() - removes the first occurrence of the item with the specified value
-'''
+# del - removes the item at the specified index
+# del list_name - deletes the list
+# pop() - removes the last item from the list
+# pop(index) - removes the item at the specified index
+# remove() - removes the first occurrence of the item with the specified value
+
 text = "a b c some other text with a b c in it"
 pattern = "a\s*b\s*c"  # This regex allows for any spaces between a, b, and c
-cleaned_text = re.sub(pattern, "", text) # This will remove all occurrences of "a b c" from the text
+cleaned_text = re.sub(pattern, "", text) # This will remove all occurrences of "a b c" from the text, regardless of the number of spaces between the letters.
 
 print(cleaned_text)
 
@@ -82,32 +81,32 @@ print(list2)
 
 list3 = list(list1) # copies the list
 print(list3)
-'''
-#You cannot copy list1 by simply assigning it to another variable. If you do so, any changes made to the new list will also affect the original list.
-#This is because both lists will point to the same memory location. To create a copy of the list, you can use the copy() method or the list() function.
-'''
+
+# You cannot copy list1 by simply assigning it to another variable. If you do so, any changes made to the new list will also affect the original list.
+# This is because both lists will point to the same memory location. To create a copy of the list, you can use the copy() method or the list() function.
+
 list1.append(11) # adds an element to the end of the list
 print(list1)
-'''
-#list((1, 2, 3, 4, 5)) # converts a tuple to a list
-#list("hello") # converts a string to a list
-#tuple([1, 2, 3, 4, 5]) # converts a list to a tuple
-#tuple("hello") # converts a string to a tuple
-#str([1, 2, 3, 4, 5]) # converts a list to a string
-'''
-Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
-Tuple items are ordered, unchangeable, and allow duplicate values.
-Tuple items are indexed, the first item has index [0], the second item has index [1] etc.
-'''
-#for loop - iterates over a sequence (list, tuple, dictionary, set, or string)
-#for each - iterates over a sequence and returns the elements one by one
-'''
+
+# list((1, 2, 3, 4, 5)) # converts a tuple to a list
+# list("hello") # converts a string to a list
+# tuple([1, 2, 3, 4, 5]) # converts a list to a tuple
+# tuple("hello") # converts a string to a tuple
+# str([1, 2, 3, 4, 5]) # converts a list to a string
+
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Tuple items are ordered, unchangeable, and allow duplicate values.
+# Tuple items are indexed, the first item has index [0], the second item has index [1] etc.
+
+# for loop - iterates over a sequence (list, tuple, dictionary, set, or string)
+# for each - iterates over a sequence and returns the elements one by one
+
 list_112 = [1, 2, 3, 4, 5]
 list_113 = ["A", "B", "C", "D", "E"]
 for x in list_112:
     print(x)
     for y in list_113:
         print(y)
-'''
-#range() - generates a sequence of numbers
-#range(start, stop, step) - generates a sequence of numbers starting from 'start', up to 'stop' (exclusive), incrementing by 'step'
+
+# range() - generates a sequence of numbers
+# range(start, stop, step) - generates a sequence of numbers starting from 'start', up to 'stop' (exclusive), incrementing by 'step'
